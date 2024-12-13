45e52096e8d32fb8bf037caabadc9feea2b0d171
1RandomDev <84292805+1RandomDev@users.noreply.github.com>
2024-12-13 21:49:21 +0100

    Add single run option

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if [ -z "${CRON_INTERVAL+x}" ]; then
-    CRON_INTERVAL="0 1 * * *"
+if [ -z "${SINGLE_RUN+x}" ]; then
+    if [ -z "${CRON_INTERVAL+x}" ]; then
+        CRON_INTERVAL="0 1 * * *"
+    fi
+    echo "$CRON_INTERVAL /usr/bin/node /app/main.js" > /etc/crontabs/root
+    crond -f
+else
+    /usr/bin/node /app/main.js
 fi
-echo "$CRON_INTERVAL /usr/bin/node /app/main.js" > /etc/crontabs/root
-crond -f
